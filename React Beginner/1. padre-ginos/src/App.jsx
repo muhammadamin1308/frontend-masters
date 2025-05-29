23f74d2045cbb05252f62951ac22608f6917fa06
Muhammadamin <79371764+muhammadamin1308@users.noreply.github.com>
2025-05-29 20:24:33 +0500

    feat: useContext-hook

--- React Beginner/1. padre-ginos/src/App.jsx
+++ React Beginner/1. padre-ginos/src/App.jsx
@@ -2,15 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
 import PizzaOfTheDay from "./PizzaOfTheDay";
+import { CartContext } from "./contexts";
 
 const App = () => {
   return (
     <StrictMode>
+      <CartContext.Provider value={cartHook}>
       <div>
         <h1 className="logo">Padre Gino's Pizza</h1>
         <Order />
         <PizzaOfTheDay />
       </div>
+      </CartContext.Provider>
     </StrictMode>
   );
 };
